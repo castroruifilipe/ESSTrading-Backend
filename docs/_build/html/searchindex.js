5bcbedbe3a26c59f9110033457b2d22cd262bb68
Rui Leite <a75551@alunos.uminho.pt>
2018-05-27 13:00:09 +0100

    docs

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndic":0,"m\u00f3dul":0,"p\u00e1gin":0,pesquis:0},titles:["Welcome to ESSTrading\u2019s documentation!"],titleterms:{and:0,documentation:0,esstrading:0,indic:0,tabl:0,welcom:0}})
